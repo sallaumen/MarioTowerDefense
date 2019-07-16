c598d4319be805196074eff238aebf83e996dea3
root <lc_tavano@hotmail.com>
2019-07-16 18:36:01 -0300

    Inserting paths and making enemies walk on them

---
+++ controller/gameController.py
@@ -0,0 +1,17 @@
+
+
+class GameController:
+
+    def __init__(self):
+        self.fps = 60
+        self.towers_position = []
+        self.enemies_positions = []
+    def start_game(self):
+        while 1:
+            pass
+
+    def create_test_game(self):
+        pass
+
+    def generate_enemy(self):
+        pass

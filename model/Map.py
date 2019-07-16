4df28a93e6e0454b9f1fdc6b6ba1daf3b93e22da
Caio <lc_tavano@hotmail.com>
2019-07-16 15:11:27 -0300

    Base map class implementation

--- model/Map.py
+++ model/Map.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Map:
     GRASS = 0
     FLOOR = 1
@@ -5,20 +7,18 @@ class Map:
     def __init__(self):
         self.lenght = 900
         self.width = 900
-        self.floor_type = [[-1] * 30]*30
+        self.floor_data = []
         self.create_empty_map()
 
     def create_empty_map(self):
-        for i in range(0, 30):
-            for j in range(0, 30):
-                self.floor_type[i][j] = self.GRASS
+        self.floor_data = np.zeros((30, 30))
 
     def get_map(self):
-        return self.floor_type
+        return self.floor_data
 
     def change_map_size(self, lenght, width):
         self.lenght = lenght
         self.width = width
 
     def create_one_floor(self, pos_x, pos_y):
-        self.floor_type[pos_x][pos_y] = self.FLOOR
+        self.floor_data[pos_x][pos_y] = self.FLOOR
--- test/MapTest.py
+++ test/MapTest.py
@@ -3,19 +3,21 @@ from model.Map import Map
 class MapTest:
 
     def __init__(self):
-        #self.test_map_creation()
+        self.test_map_creation()
         self.test_map_create_floor()
+
     def test_map_creation(self):
         map = Map()
         map.create_empty_map()
         get_map = map.get_map()
-        for i in get_map:
-            print(i)
+        for i in range(30):
+            for j in range(30):
+                assert get_map[i][j] == 0
+
 
     def test_map_create_floor(self):
         map = Map()
         map.create_empty_map()
         map.create_one_floor(29, 1)
         get_map = map.get_map()
-        for i in get_map:
-            print(i)
+        assert get_map[29][1] == 1
